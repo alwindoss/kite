0614a8013d1c4e1c3d8e939d90eb6a052d7b236e
Alwin Doss <alwindoss84@gmail.com>
2018-03-17 19:21:37 +0530

    Added test case for error condition
    Signed-off-by: Alwin Doss <alwindoss84@gmail.com>

--- margins_test.go
+++ margins_test.go
@@ -2,6 +2,7 @@ package kite_test
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -33,5 +34,42 @@ func TestMargins(t *testing.T) {
 	if err != nil {
 		t.Errorf("retuned error: %v\n", err)
 		t.Fail()
+	}
+}
+
+type MockClient struct {
+}
+
+func (c MockClient) GetMargins() (*kite.Margins, error) {
+	m := &kite.Margins{
+		Status: "error",
+	}
+	err := errors.New("unable to get margins")
+	return m, err
+}
+
+func TestMarginsWhenServerReturnsFailure(t *testing.T) {
+	// echoHandler, passes back form parameter p
+	echoHandler := func(w http.ResponseWriter, r *http.Request) {
+		margins := &kite.Margins{}
+		margins.Status = "error"
+		json.NewEncoder(w).Encode(margins)
+	}
+
+	// create test server with handler
+	ts := httptest.NewServer(http.HandlerFunc(echoHandler))
+	defer ts.Close()
+
+	// c := kite.Client{
+	// 	HTTPClient: &http.Client{},
+	// 	RootURL:    ts.URL,
+	// }
+	c := MockClient{}
+	m, err := c.GetMargins()
+	if m.Status == "error" {
+		if err == nil {
+			t.Errorf("expected error to have status but did not find error")
+			t.Fail()
+		}
 	}
 }
